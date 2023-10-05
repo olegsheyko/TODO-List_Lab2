f4ab8a08bfcdf84a28d1e7af161a4c42e5ea8d24
olegsheyko <surikat229official@gmail.com>
2023-10-05 10:12:29 +0300

    hhh

---
+++ TODO-List/Presenter/ITODO_list.cs
@@ -0,0 +1,8 @@
+﻿namespace TODO_List.Presenter;
+
+public interface ITODO_list
+{
+    void addTask(string title, string description, DateTime dateTime, string[] tags);
+    SingleTask SearchTask(string tag);
+
+}
